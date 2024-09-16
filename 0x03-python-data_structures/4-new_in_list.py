6f37bad3f18e55cbb15e99a62e0e18b17f308860
victorcreativee <tuyizerevictor@gmail.com>
2024-09-16 09:24:51 -0700

    new in list and reversed number

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (idx < 0) or (idx > len(my_list) -1):
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    temp = my_list[:]
+    if idx < 0:
+        return temp
+    if idx > len(my_list) -1:
+        return temp
+    temp[idx] = element
+    return temp
