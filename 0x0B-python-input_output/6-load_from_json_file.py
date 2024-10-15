a162d2d2a1fd6e3eece15225321d529a407135d3
victorcreativee <tuyizerevictor@gmail.com>
2024-10-15 16:12:31 -0700

    json file update

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """load from json to file"""
     with open(filename, encoding="utf-8") as file_loaded:
-        return json.load(file+loaded)
+        return json.load(file_loaded)
