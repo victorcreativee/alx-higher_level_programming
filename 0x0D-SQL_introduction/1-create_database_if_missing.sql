5d6536f1c50d44cccd3ff59bde4aab01b8759d5b
victorcreativee <tuyizerevictor@gmail.com>
2024-11-19 11:44:42 -0800

    create and delete

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create database
+-- If it is already created my command should work
+CREATE database IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Remove database if not exist ignore
+DELETE database IF NOT EXISTS hbtn_0c_0;
