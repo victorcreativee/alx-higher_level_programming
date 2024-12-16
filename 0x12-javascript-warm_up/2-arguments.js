6e71873f721b861c7004ef26ca20a766ef58fc7a
victorcreativee <tuyizerevictor@gmail.com>
2024-12-16 15:12:29 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (process.argv.length === 2) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log(process.argv[2]);
+  console.log('Arguments found');
 }
