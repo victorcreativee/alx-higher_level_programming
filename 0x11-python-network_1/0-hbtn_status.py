4253a4cbfc02228b621b7d539710ecf5939efaa0
victorcreativee <tuyizerevictor@gmail.com>
2025-01-17 20:04:43 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#a Python script that fetches https://alx-intranet.hbtn.io/status
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
