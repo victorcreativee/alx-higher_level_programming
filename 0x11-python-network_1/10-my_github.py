4d35bd50579e3d76826c61d85330b6956559a9ee
victorcreativee <tuyizerevictor@gmail.com>
2025-01-17 20:36:19 +0200

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+Usage: ./10-my_github.py
+  - You must use Basic Authentication
+"""
+
 import requests
 import sys
 
